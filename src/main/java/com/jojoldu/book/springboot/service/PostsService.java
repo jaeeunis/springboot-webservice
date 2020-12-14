034c0eaae42fd1248b822d623cc02ff81287ee70
= <jaeeunxo2@gmail.com>
2020-12-14 12:16:25 +0900

    JPA crud and mustache view

--- src/main/java/com/jojoldu/book/springboot/service/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/PostsService.java
@@ -4,11 +4,15 @@ package com.jojoldu.book.springboot.service;
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
 import com.jojoldu.book.springboot.web.dto.PostUpdateRequestDto;
+import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,6 +37,19 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(()->
                 new IllegalArgumentException("해당 게시물이 없습니다."));
         return new PostsResponseDto(entity);
+    }
+
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+       Posts posts = postsRepository.findById(id).orElseThrow(()->
+               new IllegalArgumentException("해당 아이디가 존재하지 않습니다."));
+       postsRepository.delete(posts);
     }
 
 
