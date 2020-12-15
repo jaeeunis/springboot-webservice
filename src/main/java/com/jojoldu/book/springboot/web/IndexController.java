6d0a2762372f6bb129eed68592917198c138a947
= <jaeeunxo2@gmail.com>
2020-12-15 16:34:55 +0900

    update google login and done security-login

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,22 +1,34 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+
+    private final PostsService postsService;
+
     @Autowired
-    private PostsService postsService;
+    private HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if ( user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -29,5 +41,9 @@ public class IndexController {
     public String postsUpdate(Model model,@PathVariable Long id) {
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
-        return "posts-update";}
+        return "posts-update";
+    }
+
+
+
 }
